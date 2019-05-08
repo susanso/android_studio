c0c76cb2f8d838271e6371d704d18a43f405c260
Leehyejin <hyaejin9807@gmail.com>
2019-05-08 15:38:35 +0900

    #2 프로필이미지를 추가 레이아웃도만듬

---
+++ AutoFeeder/app/src/main/java/com/hoseo/autofeeder/Profile.kt
@@ -0,0 +1,12 @@
+package com.hoseo.autofeeder
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
