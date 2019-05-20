c25b41f82ecd202e825d7e3d8cf58bc822d01fd8
Hyeonjae Kim <now97kim@naver.com>
2019-05-21 02:50:36 +0900

    #2  프로필 수정 기능 구현

--- AutoFeeder/app/src/main/java/com/hoseo/autofeeder/EditprofileActivity.kt
+++ AutoFeeder/app/src/main/java/com/hoseo/autofeeder/EditprofileActivity.kt
@@ -1,5 +1,6 @@
 package com.hoseo.autofeeder
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_editprofile.*
@@ -19,5 +20,26 @@ class EditprofileActivity : AppCompatActivity() {
             val calendarFragment = BirthdayPickerFragment()
             calendarFragment.show(fragmentManager, "EditBirthday")
         }
+
+        cancelButton.setOnClickListener {
+            startActivity(Intent(this, Profile::class.java))
+        }
+
+        applyButton.setOnClickListener {
+            val intent = Intent(this, Profile::class.java)
+
+            val name = editName.text.toString()
+            val date = editDate.text.toString()
+            val birthday = editBirthday.text.toString()
+            val weight = editWeight.text.toString()
+
+            intent.putExtra("name", name)
+            intent.putExtra("date", date)
+            intent.putExtra("birthday", birthday)
+            intent.putExtra("weight", weight)
+
+            startActivity(intent)
+        }
+
     }
 }
--- AutoFeeder/app/src/main/java/com/hoseo/autofeeder/Profile.kt
+++ AutoFeeder/app/src/main/java/com/hoseo/autofeeder/Profile.kt
@@ -3,6 +3,7 @@ package com.hoseo.autofeeder
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class Profile : AppCompatActivity() {
@@ -10,6 +11,16 @@ class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val name = intent.getStringExtra("name")
+        val date = intent.getStringExtra("date")
+        val birthday = intent.getStringExtra("birthday")
+        val weight = intent.getStringExtra("weight")
+
+        if(!TextUtils.isEmpty(name)) {textName.text = "${name}"}
+        if(!TextUtils.isEmpty(date)) {textDate.text = "${date}"}
+        if(!TextUtils.isEmpty(birthday)) {textBirthday.text = "${birthday}"}
+        if(!TextUtils.isEmpty(weight)) {textWeight.text = "${weight}g"}
 
         profileEdit.setOnClickListener {
             startActivity(Intent(this, EditprofileActivity::class.java))
