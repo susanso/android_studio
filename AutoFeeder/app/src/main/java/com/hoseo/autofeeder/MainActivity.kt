4a14683231ec3de9ef6268667081eff36a216182
kim sung eun <suanso@naver.com>
2019-05-21 21:06:08 +0900

    #1 Java 를 kotlin으로 변경중

--- AutoFeeder/app/src/main/java/com/hoseo/autofeeder/MainActivity.kt
+++ AutoFeeder/app/src/main/java/com/hoseo/autofeeder/MainActivity.kt
@@ -2,11 +2,34 @@ package com.hoseo.autofeeder
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import android.app.*;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.DatePicker.OnDateChangedListener;
+import android.widget.TimePicker.OnTimeChangedListener;
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        feed.setOnClickListener {
+            val feed_Fragment = feedTime()
+            feed_Fragment.show(fragmentManager,"main")
+            //MainActivity.kt 에서 feedTime.kt 로 이동 하는데 문제가 생김.
+        }
     }
+
+
+
 }
